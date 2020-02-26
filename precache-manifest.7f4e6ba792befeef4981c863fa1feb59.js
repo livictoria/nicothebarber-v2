d78c8b2aa458098fa8dd0b6ade5cc8b0b40732e9
livictoria <li.victoria51@gmail.com>
2020-02-25 21:49:49 -0500

    Updates

---
+++ precache-manifest.7f4e6ba792befeef4981c863fa1feb59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b0e7b03c818220b4948cd1ad9b131d",
+    "url": "/nicothebarber-v2/index.html"
+  },
+  {
+    "revision": "1b5e9d08be22f94879f2",
+    "url": "/nicothebarber-v2/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "a4508cbf4ae3c9007efe",
+    "url": "/nicothebarber-v2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b5e9d08be22f94879f2",
+    "url": "/nicothebarber-v2/static/js/2.e4e64ec3.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/nicothebarber-v2/static/js/2.e4e64ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4508cbf4ae3c9007efe",
+    "url": "/nicothebarber-v2/static/js/main.f6797e84.chunk.js"
+  },
+  {
+    "revision": "0b8754cd91236b2cf123",
+    "url": "/nicothebarber-v2/static/js/runtime-main.20fe1243.js"
+  },
+  {
+    "revision": "18ba50bc4cb0a694116108b153181c77",
+    "url": "/nicothebarber-v2/static/media/barber.18ba50bc.jpg"
+  },
+  {
+    "revision": "9bd6c7d2ff619dea95742fbb8877c505",
+    "url": "/nicothebarber-v2/static/media/nico.9bd6c7d2.svg"
+  }
+]);
